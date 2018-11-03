fa56ea8a350098f6102a54aa64427eec0d5814f7
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2018-11-04 02:16:57 +0300

    Add update.sh script

---
+++ update.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# update App Store apps
+sudo softwareupdate -i -a
+
+# update Homebrew (Cask) & packages
+brew update
+brew upgrade
+brew cask upgrade
+
+# update npm & packages
+npm install npm -g
+npm update -g
+
+# update Ruby & gems
+gem update —-system
+gem update
